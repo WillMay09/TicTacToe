8193431c9566d92a7f870109073493991ba05578
WilMay <wmayhood@outlook.com>
2024-09-10 12:38:34 -0400

    Organized the js code and added comments to make the code more readable

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const gameBoard = () =>{
 
     }
 
+    //takes the index present on each html div and translates this number into a location on the 2d array
     const updateTile = (activePlayer,index) =>{
 
         const playerLocation = translateIndex(index) 
@@ -35,7 +36,7 @@ const gameBoard = () =>{
        
 
     }
-    const getBoard = () => board;
+    
 
     const checkwin = (activePlayer) =>{
 
@@ -66,7 +67,7 @@ const gameBoard = () =>{
 
 
     }
-
+    //sets all values of the 2d array to 1
     const reset = () =>{
 
         board.map((row) =>{ row.map((cell) => {cell.changeValue("1")})});
@@ -88,6 +89,7 @@ const gameBoard = () =>{
 
 }
 
+//each cell is contained within an index of the board 2d array
 const Cell = () => {
     let value ="1";
     const getValue = ()=>value;
@@ -169,97 +171,88 @@ const translateIndex = (index) =>{
 }
 
 
-
+//controls flow of the game
 const gameController = () => {
 
    
     const playGame = gameBoard();
+    let winner = false;
+    const players = ["X", "O"];
+    let activePlayer = players[0];//X's always starts
+
+ 
+    const getActivePlayer = () => activePlayer;
+
+
+    const switchTurns = () => {
+
+        if(activePlayer === players[0]){
+            activePlayer = players[1];
+            console.log("O's turn");
+        }else{
+            activePlayer = players[0];
+            console.log("X's turn");
+        }
+    }
+    //sets the winner back to false and calls the reset method
+    const newGame = () =>{
+
+            activePlayer = players[0];
+            winner = false;
+            playGame.reset();
+    }
+
+  //updates the cells in the 2d array with either X or O
+  //calls setWinner to check if a player won and updates the players turn with switchTurns
+    const updateboard = (index) => {
+
+        playGame.updateTile(activePlayer, index);
+        playGame.printBoard();
+        setWinner();
+        switchTurns();
+        
+    }
+//checks for a winner, sets the variable to true if there is a winner
+    const setWinner = () => {
+        if(playGame.checkwin(activePlayer) === true){
+            winner = playGame.checkwin(activePlayer);
+        };
+        //console.log(winner);
+
+    }
+//returns the winner variable
+    const getWinner = () => {
+        return winner;
+    }
+
+ 
+ 
     
-  
-  let winner = false;
-  const players = ["X", "O"];
-
-  let activePlayer = players[0];
-
-  const getActivePlayer = () => activePlayer;
+    return{
 
 
-
-  const switchTurns = () => {
-
-    if(activePlayer === players[0]){
-
-        activePlayer = players[1];
-        console.log("O's turn");
-    }else{
-
-
-        activePlayer = players[0];
-        console.log("X's turn");
+        getWinner,switchTurns, updateboard, getActivePlayer, newGame
     }
 
 
-  }
-
-  const newGame = () =>{
-
-        activePlayer = players[0];
-        playGame.reset();
-  }
-
-  const updateboard = (index) => {
-
-    
-    playGame.updateTile(activePlayer, index);//updating tile with activePlayer(either X or O)
-    
-    
-    playGame.printBoard();
-    
-    setWinner();
-    switchTurns();
-   
-    //const updateLocation = writePlayerMove(currentPlayer, playerLocation);
-
-  }
-
-  const setWinner = () => {//checks for a winner, sets the variable to true if there is a winner
-
-    if(playGame.checkwin(activePlayer) === true){
-        winner = playGame.checkwin(activePlayer);
-    };
-    console.log(winner);
-
-
-  }
-
-  const getWinner = () => {//returns the winner variable
-
-    
-    return winner;
-    
-
-  }
-
- 
- 
-    
-return{
-
-
-    getWinner,switchTurns, updateboard, getActivePlayer, getBoard : playGame.getBoard
 }
 
-
-}
+//connects the backend logic of the game to the UI
 function ScreenController(){
     const startGame = gameController();
-    
     const tiles = document.querySelectorAll('.tile');
     const title = document.querySelector('.title');
-    const headerElement = document.querySelector('header');
+    const resetButton = document.querySelector('.reset');
+    let activeGame = true;
+
+    //event listener on each tile
+    //when a tile is clicked, startGame object updates the backend logic writing X or O on the clicked tile
+    //calls displayWinner to display the winner to the user if the winner variable returns true
     tiles.forEach(tile =>{
 
         tile.addEventListener('click',(event)=>{
+
+            if(!activeGame) return;
 
             const cell = event.target;//get clicked on cell
             const index = cell.getAttribute('data-index');//get index specific cell
@@ -267,50 +260,54 @@ function ScreenController(){
                 
                 cell.textContent = startGame.getActivePlayer();//update html for X or O in the tile clicked on
                 startGame.updateboard(index);//update backend board
-                displayWinner()//check for winner
-                
-                
-
-
-                
+                displayWinner()
             }
-            
-
         })
-
-
     })
 
+    //reset button calls resetGame
+    resetButton.addEventListener('click', (event)=>{
+
+            resetGame();
+
+    });
+
+/////////////////////Methods////////////////////////////////////
+
+//grabs winner variable from startGame
+//if the variable is true, stops the player from clicking on tiles and calls animateWinner
     const displayWinner = () =>{
 
         const playerWon = startGame.getWinner();
         console.log(`playerWon variable ${playerWon}`);
         if(playerWon === true){
+            activeGame = false;
             startGame.switchTurns();
             animateWinner(startGame.getActivePlayer());
         }
 
 
     }
-
+    //Changes the DOM to display the winner
     const animateWinner = (winner) =>{
-
         title.textContent = `${winner}'s wins!`;
-        headerElement.classList.remove('animate');
-
-
-       
-            headerElement.classList.add('animate')
-        
-
-
+       // headerElement.classList.remove('animate');
+        // headerElement.classList.add('animate')
 
     };
+    //resets the DOM tiles and backend logic using startGame.newGame()
+    const resetGame = () =>{
+        if(!activeGame){
+            activeGame = true;
+        }
+        title.textContent = 'Welcome to TicTacToe';
+        startGame.newGame();
+        tiles.forEach(tile=>{
 
+            tile.textContent = '';
+        });
 
-    //startGame.playRound();
-
-
+    };
 
 }
 
